540be6bd8bf1ff5722e3906c9dc1ece722ced34e
gallo <alex@entreprenerd.xyz>
2024-09-11 10:38:03 +0200

    chore: bug repro

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import {BaseSetup} from "@chimera/BaseSetup.sol";
-import "src/Counter.sol";
+import {ActivePool} from "@bold/ActivePool.sol";
 
 abstract contract Setup is BaseSetup {
-    Counter counter;
+
 
     function setup() internal virtual override {
-        counter = new Counter();
+
     }
 }
