2be32edf01eef8cd12e4c495de66487935efb0d9
grandizzy <grandizzy.the.egg@gmail.com>
2025-02-07 10:42:22 +0200

    Fix conflicting remappings

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.0;
 
 import {BaseSetup} from "@chimera/BaseSetup.sol";
 import {ActivePool} from "@bold/ActivePool.sol";
+import {Counter} from "src/Counter.sol";
 
 abstract contract Setup is BaseSetup {
 
-
+    Counter counter;
     function setup() internal virtual override {
-
+        counter = new Counter();
     }
 }
